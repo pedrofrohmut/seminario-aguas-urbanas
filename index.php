9765a9fac8f32f1e57bc5aaa5d9c68b91be27b93
pedrofrohmut <pedrofrohmut@gmail.com>
2018-04-17 16:24:49 -0300

    Connection Update 1

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 #### TESTES #############################################
-$host = "ec2-54-83-204-6.compute-1.amazonaws.com";
-$database = "d68qibg32f3fit";
-$user = "udrraepvpvfnyx";
-$port = "5432";
-$password = "3536b8b6b3a5d887eaf7e979a7a0809565036e7a9f7bc0ce008e742cf6a2539c";
-$uri = "postgres://udrraepvpvfnyx:3536b8b6b3a5d887eaf7e979a7a0809565036e7a9f7bc0ce008e742cf6a2539c@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d68qibg32f3fit";
-$herokuCli = "heroku pg:psql postgresql-cubic-10865 --app protected-stream-34303";
-
-$connectionString = "pgsql:host={$host};port={$port};dbname={$database};user={$user};password={$password}";
-
-try {
-  $conn = new PDO($connectionString);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Heroku PostgreSQL Connected Successfully";
-} catch (PDOException $e) {
-  echo "Connection failed: {$e->getMessage()}";
-}
+// $host = "ec2-54-83-204-6.compute-1.amazonaws.com";
+// $database = "d68qibg32f3fit";
+// $user = "udrraepvpvfnyx";
+// $port = "5432";
+// $password = "3536b8b6b3a5d887eaf7e979a7a0809565036e7a9f7bc0ce008e742cf6a2539c";
+// $uri = "postgres://udrraepvpvfnyx:3536b8b6b3a5d887eaf7e979a7a0809565036e7a9f7bc0ce008e742cf6a2539c@ec2-54-83-204-6.compute-1.amazonaws.com:5432/d68qibg32f3fit";
+// $herokuCli = "heroku pg:psql postgresql-cubic-10865 --app protected-stream-34303";
+//
+// $connectionString = "pgsql:host={$host};port={$port};dbname={$database};user={$user};password={$password}";
+//
+// try {
+//   $conn = new PDO($connectionString);
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   echo "Heroku PostgreSQL Connected Successfully";
+// } catch (PDOException $e) {
+//   echo "Connection failed: {$e->getMessage()}";
+// }
 #### TESTES #############################################
 
 
---
+++ sql/postgresql-script.sql
@@ -0,0 +1,28 @@
+/*** PostgreSQL ***/
+DROP TABLE IF EXISTS inscricao;
+
+CREATE TABLE inscricao (
+  /* IDETIFICAÇÃO */
+  id                BIGSERIAL  NOT NULL /*AUTO_INCREMENT*/,
+  nome              CHAR(255)  NOT NULL,
+  dataNascimento    DATE       NOT NULL,
+  rg                CHAR(255)  NOT NULL,
+  cpf               CHAR(255)  NOT NULL,
+  profissao         CHAR(255)  NOT NULL,
+  /* ENDEREÇO */
+  endereco          CHAR(255)  NOT NULL,
+  endNumero         BIGINT     NOT NULL,
+  complemento       CHAR(255),
+  bairro            CHAR(255),
+  cidade            CHAR(255)  NOT NULL,
+  uf                CHAR(255)  NOT NULL,
+  cep               CHAR(255)  NOT NULL,
+  /* CONSTATO */
+  telefone          CHAR(255)  NOT NULL,
+  outroTelefone     CHAR(255)  NOT NULL,
+  email             CHAR(255)  NOT NULL,
+  /* LOG */
+  createDate        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  /* KEYS */
+  PRIMARY KEY (id)
+);
