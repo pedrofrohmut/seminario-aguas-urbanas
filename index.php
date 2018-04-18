0add7804bf154d1f40831894514e634103e82a3f
Pedro Frohmut <pedrofrohmut@gmail.com>
2018-04-17 23:30:32 -0300

    rollback Procfile

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
+require_once "model/Inscricao.php";
+require_once "dao/InscricaoDao.php";
+require_once "banco/ConnectionFactory.php";
+
 if (ConnectionFactory::getConnection()) {
   echo "Connected successfully";
 } else {
   echo "Connection failed";
 }
-
-
-require_once "../model/Inscricao.php";
-require_once "../dao/InscricaoDao.php";
-require_once "../banco/ConnectionFactory.php";
 
 if (isset($_POST['submit'])) {
   $inscricao = new Inscricao();
